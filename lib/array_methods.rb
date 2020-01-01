815b880fd081c29e5f0fb888e759dd791e99c054
Mitchell T Alton <mitchell.alton@live.com>
2020-01-01 17:45:44 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,9 +1,9 @@
 def find_element_index(array, value_to_find)
-  array.length.times { |index|
-  if array[index] == value_to_find
+  array.length.times do |index|
+   if array[index] == value_to_find
     return array[index]
+   end
   end
-  }
   nil 
 end
 
