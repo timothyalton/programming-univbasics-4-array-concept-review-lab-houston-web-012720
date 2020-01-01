35a0eea3de37e911df6c69fd262991190f67399d
Mitchell T Alton <mitchell.alton@live.com>
2020-01-01 17:38:45 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -1,8 +1,9 @@
 def find_element_index(array, value_to_find)
   array.length.times do |index|
-    if index == value_to_find
-      
-  end
+    if [index] == value_to_find
+      return [index]
+    end
+    nil 
 end
 
 def find_max_value(array)
