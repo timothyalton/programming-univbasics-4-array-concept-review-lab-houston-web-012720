d174a7c7f02e64a5163fa1c02de6428cc69a468d
Mitchell T Alton <mitchell.alton@live.com>
2020-01-01 18:20:30 +0000

    Automatically backed up by Learn

--- lib/array_methods.rb
+++ lib/array_methods.rb
@@ -18,5 +18,11 @@ def find_max_value(array)
 end
 
 def find_min_value(array)
-  # Add your solution here
+  x = array[0]
+  array.length.times { |index|
+   if array[index] < x 
+     x = array[index]
+   end 
+  }
+  x 
 end
